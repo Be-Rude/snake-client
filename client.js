d44831d48473759cd56f6ca072c07752351f8aeb
Brad Ruud <bradruud@gmail.com>
2020-03-11 23:33:17 +0000

    adding constants.js file

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, port } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.177',
-    port: 50541
+    host: IP,
+    port: port
   });
 
   conn.setEncoding('utf8'); 
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = '192.168.88.177'
+const port = 50541
+
+module.exports = {
+  IP,
+  port
+};
