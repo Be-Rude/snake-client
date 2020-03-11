1cf5fe1ec090fe39aed9243419f262bfa2b31117
Brad Ruud <bradruud@gmail.com>
2020-03-11 21:27:58 +0000

    updating client file with Move command

--- client.js
+++ client.js
@@ -9,6 +9,10 @@ const connect = function() {
   });
   conn.on('connect', () => {
     conn.write("Name: BAR");
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1000);
+    
     });
   conn.on('data', (data) => {
     console.log('Successfully connected to game server');
